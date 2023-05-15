c047582d4c54d5c04261a95ef3b8f861ee68fd28
BatyrM <meredovbatyr2001@gmail.com>
2023-05-15 13:40:51 +0500

    new bot

---
+++ src/main/java/ru/Meredov/PogrebM/PogrebMApplication.java
@@ -0,0 +1,13 @@
+package ru.Meredov.PogrebM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogrebMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogrebMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Meredov/PogrebM/PogrebMApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Meredov.PogrebM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogrebMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
