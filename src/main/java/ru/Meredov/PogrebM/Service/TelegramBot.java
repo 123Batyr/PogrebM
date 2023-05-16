3f3feb65008477e7137c454a39e53f0df4ce8470
BatyrM <meredovbatyr2001@gmail.com>
2023-05-16 14:59:08 +0500

    stage 4

--- src/main/java/ru/Meredov/PogrebM/Service/TelegramBot.java
+++ src/main/java/ru/Meredov/PogrebM/Service/TelegramBot.java
@@ -1,21 +1,54 @@
 package ru.Meredov.PogrebM.Service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.Meredov.PogrebM.Config.BotConfig;
+import ru.Meredov.PogrebM.model.User;
+import ru.Meredov.PogrebM.model.UserRepository;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+    @Autowired
+    private UserRepository userRepository;
+
     final BotConfig config;
+
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities.\n\n" +
+            "You can execute commands from the main menu on the left o by typing a command:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /mydata to see data stored about yourself\n\n" +
+            "Type /help to see this message again";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata", "get your data stored "));
+        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
+
     }
 
     @Override
@@ -37,8 +70,16 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
+
+                    registerUser(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
+
+                case "/help":
+
+                    sendMassage(chatId, HELP_TEXT);
+                    break;
+
                 default:
 
                     sendMassage(chatId, "Sorry, command was not recognized");
@@ -50,7 +91,28 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
 
     }
-        private void startCommandReceived ( long chatId, String name){
+
+    private void registerUser(Message msg) {
+
+        if (userRepository.findById(msg.getChatId()).isEmpty()){
+
+            var chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("user saved: " + user);
+        }
+    }
+
+    private void startCommandReceived ( long chatId, String name){
 
             String answer = "Привет,  " + name + ", приятно познакомиться!";
             log.info("Replied to user " + name);
---
+++ src/main/java/ru/Meredov/PogrebM/model/UserRepository.java
@@ -0,0 +1,7 @@
+package ru.Meredov.PogrebM.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
